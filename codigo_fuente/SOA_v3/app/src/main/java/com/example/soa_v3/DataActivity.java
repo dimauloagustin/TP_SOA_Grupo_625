af51a45439ca8f665f1bd7e3ab346172625594ed
dimauloagustin <dimauloagustin@gmail.com>
2020-05-25 15:58:26 -0300

    tabla para los datos
    funciona pero aun le falta amor

--- codigo_fuente/SOA_v3/app/src/main/java/com/example/soa_v3/DataActivity.java
+++ codigo_fuente/SOA_v3/app/src/main/java/com/example/soa_v3/DataActivity.java
@@ -3,11 +3,17 @@ package com.example.soa_v3;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Gravity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.GridLayout;
+import android.widget.TableLayout;
+import android.widget.TableRow;
+import android.widget.TextView;
 
 public class DataActivity extends AppCompatActivity {
     private Button btnSalir;
+    private TableLayout tblLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,6 +21,7 @@ public class DataActivity extends AppCompatActivity {
         setContentView(R.layout.activity_data);
 
         btnSalir = (Button) findViewById(R.id.btnSalir);
+        tblLayout = (TableLayout) findViewById(R.id.tvDataTableLayout);
 
 
         btnSalir.setOnClickListener(new View.OnClickListener() {
@@ -23,6 +30,24 @@ public class DataActivity extends AppCompatActivity {
                 salir();
             }
         });
+
+        addColumn("aaaa","bbbb2");
+    }
+
+    private void addColumn(String dato1, String dato2){
+        TableRow tblRow = new TableRow(this);
+        tblRow.setLayoutParams( new TableRow.LayoutParams(TableRow.LayoutParams.MATCH_PARENT,TableRow.LayoutParams.WRAP_CONTENT));
+        TextView field1 = new TextView(this);
+        field1.setLayoutParams( new TableRow.LayoutParams(TableRow.LayoutParams.WRAP_CONTENT,TableRow.LayoutParams.WRAP_CONTENT));
+        TextView field2 = new TextView(this);
+        field2.setLayoutParams( new TableRow.LayoutParams(TableRow.LayoutParams.WRAP_CONTENT,TableRow.LayoutParams.WRAP_CONTENT));
+        field1.setText(dato1);
+        field2.setText(dato2);
+
+        tblRow.addView(field1);
+        tblRow.addView(field2);
+
+        tblLayout.addView(tblRow);
     }
 
     public void salir() {
